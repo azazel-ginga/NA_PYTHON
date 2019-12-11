d40ac5929382594faf645016760065b05fda8b25
Naruterador <103258937@qq.com>
2019-12-11 09:01:03 +0800

    update

--- python/training/crazy_python/chapter10/execrise/e2.py
+++ python/training/crazy_python/chapter10/execrise/e2.py
@@ -1,5 +1,39 @@
 #coding = utf - 8
 
 '''
-给定一个字符串，该字符串只能包含数字0~9、英文逗号、英文点号,请使用英文逗号、英文点号将它们分割成多个子串
+给定一个字符串，该字符串只能包含数字0~9、英文逗号、英文点号,请使用
+英文逗号、英文点号将它们分割成多个子串
 '''
+
+import re
+
+class stringcheck(object):
+	def __init__(self,str):
+		self.str = str
+
+	def __str_check(self):
+		for i in self.str:
+			if not i.isdigit():
+				if i != ',' and i != '.':
+					return False
+			else:
+				pass
+		return True
+
+
+	def str_split(self):
+		if self.__str_check():
+			str1 = re.findall(r'(?<=,|.)\d+',self.str)
+			return str1
+		else:
+			raise TypeError("The char must be a number or ',' or '.'!")
+
+
+
+s = stringcheck('111111,1..1,1.1')
+print(s.str_split())
+
+
+
+
+
