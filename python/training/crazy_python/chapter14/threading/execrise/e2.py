8fc7044527a87ce0ff979491836a6e834489644e
Naruterador <103258937@qq.com>
2019-12-26 20:52:44 +0800

    update

--- python/training/crazy_python/chapter14/threading/execrise/e2.py
+++ python/training/crazy_python/chapter14/threading/execrise/e2.py
@@ -9,3 +9,65 @@
 编写两个线程，其中一个线程打印1～52；另一个线程打印A~Z,打印顺序是12A34B56C……
 5152Z。
 '''
+
+
+
+
+
+
+import threading
+import time
+
+class Mythread(object):
+
+	def __init__(self):
+		self.num = 0 
+		self.ord_n = 65
+		self.flag = True
+		self.cond = threading.Condition()
+
+	def Pnumber(self):
+		self.cond.acquire()
+
+		if not self.flag:
+			self.cond.wait()
+		else:
+			if self.num < 52:
+				for i in range(2):
+					self.num += 1
+					print(self.num)
+					self.flag = False
+					self.cond.notify()
+		
+		self.cond.release()
+
+
+	def Pleg(self):
+		self.cond.acquire()
+
+		if self.flag:
+			self.cond.wait()
+		else:
+			if self.num <= 52:
+				print(chr(self.ord_n))
+				self.ord_n += 1
+				self.flag = True
+				self.cond.notify()
+		
+		self.cond.release()
+
+
+
+
+th = Mythread()
+
+
+for i in range(26):
+	t1 = threading.Thread(target=th.Pnumber,args=())
+	t1.start()
+
+	t2 = threading.Thread(target=th.Pleg,args=())
+	t2.start()
+
+
+
---
+++ python/training/crazy_python/chapter14/threading/execrise/e4.py
@@ -0,0 +1,127 @@
+#coding = utf - 8
+
+
+
+
+'''
+某个博物馆最多可以容纳500人同时参观，有一个出入口，该出入口一次仅仅允许一人
+通过，参观者的活动描述如下:
+
+参观者动作:
+{
+	……
+
+	进门(entance)
+
+	……
+
+	参观(visit)
+
+	……
+
+	出门	(exit)
+	
+	……
+}
+
+请添加必要的信号量和P、V[或者wait()、signal]操作，以实现上述过程中
+的互斥与同步。要求写出完成的过程，说明信号量的含义并赋初值。
+
+'''
+
+
+
+import threading
+import sys
+
+
+class Vmuseum(object):
+    
+    def __init__(self,enter_num):
+
+        if enter_num > 500:
+            sys.exit("The museum just can contian 500 people")
+        elif enter_num <= 0:
+            sys.exit("You must let at least 1 people get into the museum!")
+        else:
+            self.enter_num = enter_num
+        self.f_enter_num = 0
+        self.all_people = enter_num
+        self.cond = threading.Condition()
+        self.flag = False
+
+    #enterance
+    def enterance(self):
+        
+        self.cond.acquire()
+        if self.flag:
+            self.cond.wait()
+        else:
+            if self.f_enter_num <= self.enter_num:
+                self.f_enter_num += 1
+                print("Now enter visiting member is %d,enter %d person" % (self.f_enter_num,1))
+                self.cond.notify()
+                self.flag = True
+
+        self.cond.release()
+
+
+
+    '''
+    #visiting
+    def visti(self):
+        pass
+    '''
+
+    #exit
+    def exit(self):
+        self.cond.acquire()
+        if not self.flag:
+            self.cond.wait()
+        else:
+            if self.enter_num >= 0:
+                self.enter_num  -= 1
+                print("Now exit visiting member is %d,exit %d person" % (self.enter_num,1))
+                self.cond.notify()
+                self.flag = False
+        
+        self.cond.release()
+
+            
+vm = Vmuseum(200)
+
+def enter(times):
+    for i in range(times):
+        vm.enterance()
+
+def exit(times):
+    for i in range(times):
+        vm.exit()
+
+threading.Thread(target=enter,args=(0,200)).start()
+threading.Thread(target=exit,args=(200,200)).start()
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
